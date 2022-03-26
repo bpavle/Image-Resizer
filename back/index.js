c3ca134e4ee55124dc729a933fa50a8ee5c132cc
bpavle <bpavle0@gmail.com>
2022-03-26 13:29:58 +0100

    feat: Added correct api endpoints

--- back/index.js
+++ back/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const { uploadFile } = require("./s3");
+const bodyParser = require("body-parser");
 var path = require("path");
 const app = express();
 const port = 9000;
 
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, "../front/build")));
 
-app.get("/api", (req, res) => {
+app.get("/api/v1/status", async (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.post("/api/v1/upload-image", async (req, res) => {
+  //const resp = await uploadFile("000000.png");
+  console.log(req.body);
   res.json({ message: "Hello from server!" });
 });
 // All other GET requests not handled before will return our React app
