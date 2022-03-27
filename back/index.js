0e35bb4c128a16bc6a742b2fd760ab07e728c483
bpavle <bpavle0@gmail.com>
2022-03-27 18:55:49 +0200

    feat: Disable cors

--- back/index.js
+++ back/index.js
@@ -9,6 +9,14 @@ var path = require("path");
 const app = express();
 const port = 9000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, "../front/build")));
 
