f9dffd0e979d9158840cd0d04b92e68f6ebb00b2
bpavle <bpavle0@gmail.com>
2022-03-27 03:35:20 +0200

    feat: Main worker file

---
+++ workers/index.js
@@ -0,0 +1,26 @@
+const { getNextTask } = require("./sqs");
+const { getImageByKey } = require("./s3");
+const { resize } = require("./resize");
+const util = require("util");
+
+const fs = require("fs");
+const unlinkFile = util.promisify(fs.unlink);
+
+(async () => {
+  const [key, size] = await getNextTask();
+  const image = await getImageByKey(key);
+
+  var dir = `${__dirname}/public/images`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+  fs.writeFileSync(dir + `/${key}`, image);
+
+  const [width, height] = size.split("x").map(Number);
+  resize(dir + `/${key}`, width, height);
+  unlinkFile(dir + `/${key}`);
+})();
+
+//TODO: Upload back to s3
+
+//TODO: Remove original from s3
