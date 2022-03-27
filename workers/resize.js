972738247d10c357f17bf58108686041cb290116
bpavle <bpavle0@gmail.com>
2022-03-27 03:32:23 +0200

    feat: Resize module

---
+++ workers/resize.js
@@ -0,0 +1,33 @@
+const sharp = require("sharp");
+const fs = require("fs");
+
+const resize = (filePath, width, height) => {
+  console.log("Resizing");
+  // input stream
+  let inStream = fs.createReadStream(filePath);
+
+  // output stream
+  let outStream = fs.createWriteStream(filePath + "resized", { flags: "w" });
+
+  // on error of output file being saved
+  outStream.on("error", function () {
+    console.log("Error");
+  });
+
+  // on success of output file being saved
+  outStream.on("close", function () {
+    console.log("Successfully saved file");
+  });
+
+  // input stream transformer
+  // "info" event will be emitted on resize
+  let transform = sharp()
+    .resize({ width: width, height: height })
+    .on("info", function (fileInfo) {
+      console.log("Resizing done, file not saved");
+    });
+
+  inStream.pipe(transform).pipe(outStream);
+};
+
+module.exports = { resize };
