80851f6f4e7813bdfc80113ffe0234a559c56631
bpavle <bpavle0@gmail.com>
2022-03-28 06:21:44 +0200

    feat: Set loader while whaiting for images to be resized

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import FileUploader from "./components/FIleUploader";
-import { Typography, Paper, Container } from "@mui/material";
+import {
+  Typography,
+  Paper,
+  Container,
+  CircularProgress,
+  Backdrop,
+} from "@mui/material";
 import FileList from "./components/FileList";
 import { ImagesProvider } from "./components/Contexts/ImagesContext";
+import { useState } from "react";
 
 function App() {
+  let [disabled, setDisabled] = useState(false);
+  if (disabled)
+    return (
+      <Backdrop sx={{ color: "#fff", zIndex: 100 }} open={true}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
   return (
     <Container
       maxWidth="xl"
@@ -24,7 +38,7 @@ function App() {
       </Typography>
       <ImagesProvider>
         <FileList></FileList>
-        <FileUploader />
+        <FileUploader setDisabled={setDisabled} />
       </ImagesProvider>
     </Container>
   );
