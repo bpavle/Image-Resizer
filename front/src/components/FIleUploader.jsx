84d1f156089ffaeda7e38268733bf20515196795
bpavle <bpavle0@gmail.com>
2022-03-28 07:19:48 +0200

    feat: Downloading bunch of files at once

--- front/src/components/FIleUploader.jsx
+++ front/src/components/FIleUploader.jsx
@@ -3,6 +3,8 @@ import { styled } from "@mui/material/styles";
 import { useContext } from "react";
 import STATUS_CODE from "../utils/statuses";
 import { ImagesContext } from "./Contexts/ImagesContext";
+import sendRequest from "../utils/try-requests";
+import multiDownload from "multi-download";
 
 const Input = styled("input")({
   display: "none",
@@ -10,6 +12,8 @@ const Input = styled("input")({
 const FileUploader = (props) => {
   let [images, setImages] = useContext(ImagesContext).images;
   let [trigger, setTrigger] = useContext(ImagesContext).triggerAll;
+
+  let arr = [];
 
   const selectFile = (event) => {
     //let files = [];
@@ -34,8 +38,16 @@ const FileUploader = (props) => {
   };
 
   const resizeImages = () => {
-    console.log("CLICKED UPLOAD");
-    setTrigger(true);
+    let interval = setInterval(() => {
+      if (arr.length === images.length) {
+        clearInterval(interval);
+        console.log("GET THEM ALL");
+        console.log(arr);
+        multiDownload(arr);
+      }
+    }, 1000);
+
+    props.setDisabled(true);
     setImages(
       images.map((image) => {
         image.status = STATUS_CODE.UPLOADING;
@@ -54,15 +66,61 @@ const FileUploader = (props) => {
         )
           .then((response) => response.text())
           .then((result) => {
-            //console.log(result);
-            console.log(JSON.parse(result).data.key);
-            image.aws_key = JSON.parse(result).data.key;
+            console.log(JSON.parse(result));
+            console.log(JSON.parse(result).data.Location);
+            let location = JSON.parse(result).data.Location;
+            let key = JSON.parse(result).data.key;
+            image.aws_key = key;
+            image.resized_location = location.replace(key, "resized_" + key);
+            console.log(image);
+            const f = () => {
+              console.log(location);
+              fetch(
+                `${process.env.REACT_APP_BACKEND_API}/status/resized_${key}`,
+                {
+                  method: "GET",
+                }
+              )
+                .then((response) => {
+                  return response.text();
+                })
+                .then((result) => {
+                  console.log(result);
+                  if (JSON.parse(result).status == "success") {
+                    arr.push(image.resized_location);
+                    clearInterval(interval);
+                  }
+                });
+            };
+            let interval = setInterval(() => {
+              f();
+            }, 1000);
           })
           .catch((error) => console.log("error", error));
         //console.log(image);
         return image;
       })
     );
+    // Promise.all(
+    //   images.map((image) => {
+    //     return fetch(
+    //       `${process.env.REACT_APP_BACKEND_API}/status/${image.resized_location}`,
+    //       {
+    //         method: "GET",
+    //       }
+    //     );
+    //   })
+    //   // images.map((image) => {
+    //   //   return sendRequest(image.resized_location);
+    //   // })
+    // )
+    //   .then((results) => {
+    //     console.log(results);
+    //     console.log("ok");
+    //     console.log(images);
+    //     //TODO: Multi download
+    //   })
+    //   .catch((results) => console.error("not ok"));
   };
   return (
     <>
