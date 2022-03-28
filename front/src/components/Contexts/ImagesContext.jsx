b0b8dad4ef881323b267f57fbcd7c08e76dc80ad
bpavle <bpavle0@gmail.com>
2022-03-28 02:47:03 +0200

    feat: Added trigger to context in attempt to fetch independent statuses for images

--- front/src/components/Contexts/ImagesContext.jsx
+++ front/src/components/Contexts/ImagesContext.jsx
@@ -4,9 +4,12 @@ export const ImagesContext = React.createContext();
 
 export const ImagesProvider = (props) => {
   const [images, setImages] = useState([]);
+  const [trigger, setTrigger] = useState(false);
   console.log(images);
   return (
-    <ImagesContext.Provider value={[images, setImages]}>
+    <ImagesContext.Provider
+      value={{ images: [images, setImages], triggerAll: [trigger, setTrigger] }}
+    >
       {props.children}
     </ImagesContext.Provider>
   );
