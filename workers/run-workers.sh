73dc4417841ed677090018518dfa83a55e17b252
bpavle <bpavle0@gmail.com>
2022-03-28 10:44:24 +0200

    fix: Prevent running dry

--- workers/run-workers.sh
+++ workers/run-workers.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+source ".env" || true
+
+if [ -z $1 ]; then
+    echo "Specify number of workers like so: ./run-workers.sh 5"
+    exit 1
+fi
+echo $NODE_EXE
 sudo cp resize-worker.target /etc/systemd/system/
 sudo cp resize-worker@.service /etc/systemd/system/
 NUMBER_OF_WORKERS="${1}"
@@ -9,6 +17,9 @@ done
 echo Services: "$SERVICES"
 echo NUMBER_OF_WORKERS: "$NUMBER_OF_WORKERS"
 sudo sed -i "s/.*Requires=.*/$SERVICES/" /etc/systemd/system/resize-worker.target
+sudo sed -i "s/NODE/$NODE_EXE/" /etc/systemd/system/resize-worker@.service
+sudo sed -i "s/WORKER_DIR/$WORKER_DIR\/.env/" /etc/systemd/system/resize-worker@.service
+sudo sed -i "s/WORKER/$WORKER/" /etc/systemd/system/resize-worker@.service
 
 sudo systemctl daemon-reload
 sudo systemctl start resize-worker.target
